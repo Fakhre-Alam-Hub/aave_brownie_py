a32cdc88cdcef2452213241e4e53f2de4b8bf46f
Fakhre Alam <fakhrealam9537@gmail.com>
2022-05-31 12:17:05 +0530

    Complted Defi with Aave using brownie and chainlink

---
+++ README.md
@@ -0,0 +1,11 @@
+1. Swap our ETH to WETH
+2. Deposit some ETH (WETH) into AAVE
+3. Borrow some asset with the ETH collateral
+    - Sell that borrowed asset. (short selling)
+4. Repay everything back
+
+
+Testing:
+
+Integration test: kovan
+Unit tests: mainnet-fork
---
+++ scripts/get_weth.py
@@ -0,0 +1,23 @@
+from brownie import accounts, config, network, interface
+from scripts.helpful_scripts import get_account
+
+def main():
+    """
+    Runs the get_weth function to get WETH
+    """
+    get_weth()
+
+
+def get_weth(account=None):
+    """
+    Mints WETH by depositing ETH.
+    """
+    account = get_account()
+
+    weth = interface.IWeth(
+        config["networks"][network.show_active()]["weth_token"]
+    )
+    tx = weth.deposit({"from": account, "value": 0.1 * 1e18})
+    tx.wait(1)
+    print("Received 0.1 WETH")
+    return tx
